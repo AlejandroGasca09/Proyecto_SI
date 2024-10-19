faf05d380ef89842d621733e3707f565a20ad20f
Abraham-MF <abraham.morales2600@gmail.com>
2024-10-18 20:54:32 -0600

    en

---
+++ footer.php
@@ -0,0 +1,32 @@
+<footer class="page-footer orange lighten-1">
+    <div class="container">
+      <div class="row">
+        <div class="col l6 s12">
+          <h5 class="white-text">FES ARAGON</h5>
+          <p class="grey-text text-lighten-4">Facultad de Estudios Superiores Aragon.</p>
+        </div>
+        <div class="col l4 offset-l2 s12">
+          <h5 class="white-text"> ¡¡¡ Nuestras redes !!!</h5>
+          <ul>
+            <li><a class="grey-text text-lighten-3" href="https://www.facebook.com/FESAragonUNAM/">facebook FES
+                Aragon</a></li>
+            <li><a class="grey-text text-lighten-3" href="https://www.facebook.com/FESAragon.computacion">facebook
+                Ingenieria en Computacion</a></li>
+            <li><a class="grey-text text-lighten-3" href="https://www.instagram.com/fesaragonunam/">instagram FES
+                Aragon</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="footer-copyright">
+      <div class="container">
+        © 2014 Copyright Text
+        <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+      </div>
+    </div>
+  </footer>
+</body>
+<script>
+  M.AutoInit();
+</script>
+</html>
