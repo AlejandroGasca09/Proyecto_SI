b3cdbca94ed4675d168ca78322571a77f5513613
AlejandroGasca09 <119777270+AlejandroGasca09@users.noreply.github.com>
2024-10-20 12:21:17 -0600

    Add files via upload

--- Proyecto_SI/index.php
+++ Proyecto_SI/index.php
@@ -1,33 +1,36 @@
 <?php
 include"./header.php"
 ?>
-
+    <ul id="slide-out" class="sidenav">
+      <li>
+        <div class="user-view">
+          <div class="background">
+            <img src="media/amiguito.png">
+          </div>
+          <a href="#user"><img class="circle" src="media/perfil.jpg"></a>
+          <a href="#name"><span class="white-text name">Pumamigo</span></a>
+          <a href="#email"><span class="white-text email">Pumamigo@gmail.com</span></a>
+        </div>
+      </li>
+      <li><a href="#!"><i class="material-icons">cloud</i>First Link With Icon</a></li>
+      <li><a href="#!">Second Link</a></li>
+      <li>
+        <div class="divider"></div>
+      </li>
+      <li><a class="subheader">Subheader</a></li>
+      <li><a class="waves-effect" href="#!">Third Link With Waves</a></li>
+    </ul>
+    <a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></a>
 
   </header>
   <div class="container">
-    <h1>OPCIONES DE TITULACIÓN</h1>
+    <h1>OPCIONES DE TITULACION</h1>
     <p>La FES Aragón ofrece diversas opciones de titulación que permiten a los estudiantes elegir la modalidad que mejor
       se adapte a sus necesidades y preferencias. Es importante consultar con la coordinación de cada carrera para
       conocer los requisitos específicos y detalles adicionales.</p>
     <ul class="collapsible">
       <li>
-        <div class="collapsible-header"><i class="material-icons">class</i>PLAN DE ESTUDIOS 1981</div>
-        <div class="collapsible-body" ><span>El plan de estudios de la Licenciatura en Ingeniería en Computación
-            (clave 0674) de la Universidad Nacional Autónoma de México (UNAM) se inició en 1981. Este programa tiene una
-            duración de 9 semestres y requiere completar 380 créditos obligatorios y 32 créditos optativos.
-            No hay límite de inscripciones por periodo y el plan de estudios no es vigente.</span><a
-            href="plan1.html">Conoce los requisitos de titulacion</a></div>
-      </li>
-      <li>
-        <div class="collapsible-header"><i class="material-icons">class</i>PLAN DE ESTUDIOS 1984</div>
-        <div class="collapsible-body"><span>El plan de estudios de la Licenciatura en Ingeniería en Computación
-            (clave 0080) de la Universidad Nacional Autónoma de México (UNAM) se inició en 1984. Este programa tiene una
-            duración de 9 semestres y requiere completar 380 créditos obligatorios y 38 créditos optativos.
-            No hay límite de inscripciones por periodo y el plan de estudios no es vigente</span><a
-            href="plan1.html">Conoce los requisitos de titulacion</a></div>
-      </li>
-      <li>
-        <div class="collapsible-header"><i class="material-icons">class</i>PLAN DE ESTUDIOS 1992</div>
+        <div class="collapsible-header"><i class="material-icons">class</i>PLAN DE ESTUDIOS 0082</div>
         <div class="collapsible-body"><span>El plan de estudios de la Licenciatura en Ingeniería en Computación
             (clave 0082) de la Universidad Nacional Autónoma de México (UNAM) se inició en 1992. Este programa tiene una
             duración de 10 semestres y requiere completar 386 créditos obligatorios y 32 créditos optativos.
@@ -35,7 +38,7 @@ include"./header.php"
             href="plan1.html">Conoce los requisitos de titulacion</a></div>
       </li>
       <li>
-        <div class="collapsible-header"><i class="material-icons">class</i>PLAN DE ESTUDIOS 2008</div>
+        <div class="collapsible-header"><i class="material-icons">class</i>PLAN DE ESTUDIOS 1979</div>
         <div class="collapsible-body"><span>El plan de estudios de la Licenciatura en Ingeniería en Computación
             (clave 1279) de la Universidad Nacional Autónoma de México (UNAM) se inició en 2008. Este programa tiene una
             duración de 9 semestres y requiere completar 370 créditos obligatorios y 32 créditos optativos.
@@ -44,7 +47,7 @@ include"./header.php"
             href="plan1.html">Conoce los requisitos de titulacion</a></div>
       </li>
       <li>
-        <div class="collapsible-header"><i class="material-icons">class</i>PLAN DE ESTUDIOS 2020</div>
+        <div class="collapsible-header"><i class="material-icons">class</i>PLAN DE ESTUDIOS 2129</div>
         <div class="collapsible-body"><span>El plan de estudios de la Licenciatura en Ingeniería en Computación
             (clave 2119) de la Universidad Nacional Autónoma de México (UNAM) se inició en 2020. Este programa tiene una
             duración de 9 semestres y requiere completar 348 créditos obligatorios y 48 créditos optativos.
